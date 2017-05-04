37424da2f42b4b68e2b4420a2d4101896912f19a
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-05-04 09:45:33 +0300

    Simple webpack-react-hot application

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: resolve(__dirname, 'src'),
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [ 'babel-loader', ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader?modules', ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+};
